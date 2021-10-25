0b3dbaeb25f6ee3e9095a3a622ecf6c4cf17e977
Jordi Cidoncha <jordi.cidoncha@itb.cat>
2021-10-25 15:18:32 +0200

    Initial commit

---
+++ src/main/java/cat/itb/helloworld/HelloWorldController.java
@@ -0,0 +1,13 @@
+package cat.itb.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping
+    public String HelloWorld() {
+        return "Hello World!";
+    }
+}
